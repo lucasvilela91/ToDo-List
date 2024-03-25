99753dedafa251be4883b6efe9b3e5af1826c191
Lucas Freitas Vilela <lucasvilelapersonal@gmail.com>
2024-03-25 16:06:52 -0300

    Criados componente

--- vite-project/src/components/index.js
+++ vite-project/src/components/index.js
@@ -2,5 +2,6 @@ export * from './Botao';
 export * from './Cabecalho';
 export * from './CampoTexto';
 export * from './Conteudo';
-export * from './FormCriarTarefa'
+export * from './FormCriarTarefa';
 export * from './Rodape';
+export * from './ListaTarefas';
