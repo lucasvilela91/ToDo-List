2b047bb11cab9c54f99eab3712b1a5edada9f676
Lucas Freitas Vilela <lucasvilelapersonal@gmail.com>
2024-03-27 10:38:52 -0300

    Listando e criando tarefas, usando o gerenciamento estado local

--- vite-project/src/components/FormCriarTarefa/FormCriarTarefa.jsx
+++ vite-project/src/components/FormCriarTarefa/FormCriarTarefa.jsx
@@ -1,14 +1,46 @@
+import { useState } from "react";
+
 import { Botao, CampoTexto, TIPO_BOTAO } from "../../components";
 
-import style from './FormCriarTarefa.module.css';
+import style from "./FormCriarTarefa.module.css";
 
-const FormCriarTarefa = () => {
-    return (
-        <form className={style.FormCriarTarefa}>
-            <CampoTexto />
-            <Botao texto="+" />
-        </form>
-    );
+const FormCriarTarefa = (props) => {
+  const [nomeTarefa, setNomeTarefa] = useState("");
+
+  const { setTarefas } = props;
+
+  const onChangeNomeTarefa = (event) => {
+    setNomeTarefa(event.currentTarget.value);
+  };
+
+  const adicionarTarefa = (event) => {
+    event.preventDefault();
+
+    if (!nomeTarefa) {
+        return;
+    }
+
+    setTarefas(estadoAtual => {
+        const tarefa = {
+            id: estadoAtual.length + 1,
+            nome: nomeTarefa,
+        }
+
+        return [
+            ...estadoAtual,
+            tarefa,
+        ];
+    });
+
+    setNomeTarefa('');
+  };
+
+  return (
+    <form className={style.FormCriarTarefa} onSubmit={adicionarTarefa}>
+      <CampoTexto value={nomeTarefa} onChange={onChangeNomeTarefa} />
+      <Botao texto="+" />
+    </form>
+  );
 };
 
 export { FormCriarTarefa };
--- vite-project/src/components/ListaTarefas/ListaTarefas.jsx
+++ vite-project/src/components/ListaTarefas/ListaTarefas.jsx
@@ -3,12 +3,9 @@ import { ListaTarefasItem } from "./ListaTarefasItem";
 import style from './ListaTarefas.module.css';
 
 
-const ListaTarefas = () => {
-    const tarefas = [
-        {id: 1, nome: 'Item 1' },
-        {id: 2, nome: 'Item 2' },
-        {id: 3, nome: 'Item 3' },
-    ];
+const ListaTarefas = (props) => {
+    const { tarefas } = props;
+
     return (
         <ul className={style.ListaTarefas}>
             {tarefas.map(item => <ListaTarefasItem key={item.id} nome={item.nome} />)}
