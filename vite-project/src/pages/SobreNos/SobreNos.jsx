4e47e4ad53678a78ea52cb7a10c59fad46287c5b
Lucas Freitas Vilela <lucasvilelapersonal@gmail.com>
2024-05-02 11:47:38 -0300

    Feat: Created about us

--- vite-project/src/pages/SobreNos/SobreNos.jsx
+++ vite-project/src/pages/SobreNos/SobreNos.jsx
@@ -1,7 +1,34 @@
+import style from "./SobreNos.module.css";
+
 const SobreNos = () => {
-    return (
-        <h1>Sobre Nós</h1>
-    );
+  return (
+    <div className={style.sobreNos}>
+      <h1>Sobre Nós</h1>
+
+      <br />
+      <p>
+        Apaixonado por tecnologia e computadores desde a infância, sou estudante
+        de engenharia de software com foco em desenvolvimento front-end.
+      </p>
+      <p>
+        Possuo conhecimento em MySQL, Git, CSS, HTML e JavaScript, e busco
+        constante aprimoramento. Sou autodidata e tenho foco no aprendizado de
+        frameworks como React.
+      </p>
+      <p>
+        {" "}
+        Tenho conhecimento básico de Linux e estudo React nas horas vagas. Busco
+        uma oportunidade na área de desenvolvimento web, e estou disposto a
+        contribuir para projetos desafiadores com dedicação e entusiasmo.{" "}
+      </p>
+
+      <br />
+      <p>Email de contato:</p>
+      <a href="mailto:lucasvilelapersonal@gmail.com" target="_blank">
+        lucasvilelapersonal@gmail.com
+      </a>
+    </div>
+  );
 };
 
 export { SobreNos };
